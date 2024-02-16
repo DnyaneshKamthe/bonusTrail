a8fa530d32376e890f77c71f8240a6280c0abf44
harshal-kitukale <harshalkitukale005@gmail.com>
2024-02-16 13:19:53 +0530

    added condition for userid

--- Controllers/users.js
+++ Controllers/users.js
@@ -5,6 +5,11 @@ const { UserMaster } = require("../models/bonusTrail.game");
 // const {GameUser}
 const registerUser = async (userId, socket) => {
   try {
+    if(!userId){
+      console.log({ msg: "error in register user:- userId not found" });
+      // callback("userId not found")
+      return
+    }
     const user = await UserMaster.findOne({_id:userId});
 
     if (!user) {
@@ -23,6 +28,10 @@ const registerUser = async (userId, socket) => {
 
 const updatedUserAfterWin =(userId,socket)=>{
     socket.on("getUpdatedUserDetails",async()=>{
+      if(!userId){
+        console.log({msg:'user not found in updatedUserAfterWin'})
+        return
+      }
       let user = await UserMaster.findOne({ _id: userId });
         if(!user){
           console.log({msg:'user not found'})
